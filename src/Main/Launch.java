17c7cbf1e0bb0169e5d37914d2094994f89383a7
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-08-01 23:01:04 +0800

    Update Launch.java

--- src/Main/Launch.java
+++ src/Main/Launch.java
@@ -1,1 +1,24 @@
+package Main;
 
+public class Launch {
+	
+	private final String title="Maze";
+	
+	private final int width=800;
+	
+	private final int height=800;
+	
+	public Window window;
+	
+	private Display display;
+	
+	public static void main(String[]args) {
+		new Launch();
+	}
+	
+	private Launch() {
+		window=new Window(title,width,height);
+		display=new Display(window);
+		display.start();
+	}
+}
