fc5505aed6fa2bc323389afb6fb9acaf2372c9a0
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-08-01 23:19:46 +0800

    Update Wall.java

--- src/Entity/Wall.java
+++ src/Entity/Wall.java
@@ -1,1 +1,24 @@
+package Entity;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import Main.Display;
+
+public class Wall extends Entity {
+	
+	private final Color color=Color.GRAY;
+	
+	public Wall(Display display,int x,int y) {
+		super(display,x,y);
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(color);
+		g.fillRect(x,y,display.getResolution(),display.getResolution());
+	}
+}
