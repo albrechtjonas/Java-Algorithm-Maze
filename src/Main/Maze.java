3cdd7de0b61bb8d75e3593818386a6fbaa8af169
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-23 12:53:26 -0700

    Update and rename Launch.java to Maze.java

--- src/Main/Maze.java
+++ src/Main/Maze.java
@@ -1,7 +1,6 @@
 package Main;
 
-public class Launch {
-	
+public class Maze {
 	private final String title="Maze";
 	
 	private final int width=800;
@@ -13,11 +12,12 @@ public class Launch {
 	private Display display;
 	
 	public static void main(String[]args) {
-		new Launch();
+		new Maze();
 	}
 	
-	private Launch() {
+	private Maze() {
 		window=new Window(title,width,height);
+		
 		display=new Display(window);
 		display.start();
 	}
