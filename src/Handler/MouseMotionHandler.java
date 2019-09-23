56aeaad06d4ac126bc6d2402358d37fe63ccf19a
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-23 12:57:40 -0700

    Update MouseMotionHandler.java

--- src/Handler/MouseMotionHandler.java
+++ src/Handler/MouseMotionHandler.java
@@ -3,11 +3,15 @@ package Handler;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
-public class MouseMotionHandler implements MouseMotionListener{
+public class MouseMotionHandler implements MouseMotionListener {
 
 	private int x;
 	
 	private int y;
+	
+	public MouseMotionHandler() {
+		
+	}
 	
 	public void mouseDragged(MouseEvent event) {
 		x=event.getX();
