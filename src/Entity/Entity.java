a2a78ac0f569081217bf55cd3799ae2fd2cb132a
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-23 12:58:48 -0700

    Update Entity.java

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,10 +1,9 @@
 package Entity;
 
-import java.awt.Graphics2D;
-
 import Main.Display;
 
 public abstract class Entity {
+	
 	protected Display display;
 	
 	protected int x;
@@ -16,10 +15,6 @@ public abstract class Entity {
 		this.x=x;
 		this.y=y;
 	}
-	
-	public abstract void tick();
-	
-	public abstract void render(Graphics2D g);
 	
 	public void setX(int x) {
 		this.x=x;
