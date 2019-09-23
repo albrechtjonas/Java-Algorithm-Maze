f65f7eae40e82c15181771db61d2976ee70e9c6a
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-23 12:55:19 -0700

    Update RunState.java

--- src/State/RunState.java
+++ src/State/RunState.java
@@ -3,32 +3,34 @@ package State;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
-import java.util.LinkedList;
 
-import Core.Maker.DFSMaker;
-import Core.Solver.AStarSolver;
-import Core.Solver.BFSSolver;
+import Entity.Entrance;
 import Entity.Exit;
-import Entity.Maker;
 import Entity.Path;
 import Entity.Wall;
+import Interface.Renderable;
+import Interface.Tickable;
 import Main.Display;
+import Runner.Maker.DFSMaker;
+import Runner.Solver.BFSSolver;
 
-public class RunState extends State {
+public class RunState extends State implements Tickable,Renderable {
 	
-	private LinkedList<Path> paths=new LinkedList<Path>();
+	private boolean DFSMaking;
 	
-	private ArrayList<Wall> walls=new ArrayList<Wall>();
-	
-	private Maker maker;
-	
-	private Exit exit;
+	private boolean BFSSolving;
 	
 	private DFSMaker dFSMaker;
 	
 	private BFSSolver bFSSolver;
 	
-	private AStarSolver aStarSolver;
+	private Entrance entrance;
+	
+	private Exit exit;
+	
+	private ArrayList<Wall> walls=new ArrayList<Wall>();
+	
+	private ArrayList<Path> paths=new ArrayList<Path>();
 	
 	public RunState(Display display) {
 		super(display);
@@ -37,58 +39,93 @@ public class RunState extends State {
 	}
 	
 	private void createObject() {
-		maker=new Maker(display,0,0);
-		
+		entrance=new Entrance(display,0,0);
 		exit=new Exit(display,0,0);
 	}
 	
 	public void tick() {
-		for(int i=0;i<paths.size();i++) {
-			try {
-				paths.get(i).tick();
-			}catch(Exception exception) {}
-		}
+		DFSMakerTick();
 		
-		for(int i=0;i<walls.size();i++) {
-			try {
-				walls.get(i).tick();
-			}catch(Exception exception) {}
-		}
+		BFSSolverTick();
 		
-		maker.tick();
+		entrance.tick();
 		
 		exit.tick();
 		
-		if(display.getStartMaking()==true) {
-			dFSMaker=new DFSMaker(display);
-			dFSMaker.start();
-			display.setStartMaking(false);
-		}
-		
-		if(display.getStartSolving()==true) {
-			bFSSolver=new BFSSolver(display);
-			bFSSolver.start();
-			display.setStartSolving(false);
-		}
-		
-		if(display.getStart()==true) {
-			aStarSolver=new AStarSolver(display);
-			aStarSolver.start();
-			display.setStart(false);
+		for(int i=0;i<walls.size();i++) {
+			walls.get(i).tick();
 		}
 	}
 	
-	public void render(Graphics2D g) {
-		for(int i=0;i<paths.size();i++) {
-			try {
-				paths.get(i).render(g);
-			}catch(Exception exception) {}
+	private void DFSMakerTick() {
+		if(DFSMaking==true) {
+			dFSMaker=new DFSMaker(display);
+			dFSMaker.start();
+			DFSMaking=false;
 		}
 		
+		if(dFSMaker!=null && dFSMaker.getDone()) {
+			boolean[][] map=dFSMaker.getMap();
+			
+			for(int x=0;x<map.length;x++) {
+				for(int y=0;y<map.length;y++) {
+					if(map[x][y]==false) {
+						walls.add(new Wall(display,x*display.getResolution(),y*display.getResolution()));
+					}
+				}
+			}
+			dFSMaker.stop();
+			dFSMaker=null;
+		}
+	}
+	
+	private void BFSSolverTick() {
+		if(BFSSolving==true) {
+			bFSSolver=new BFSSolver(display);
+			bFSSolver.start();
+			BFSSolving=false;
+		}
+		
+		if(bFSSolver!=null && bFSSolver.getDone() && bFSSolver.getSolution()!=null) {
+			int[] solution=bFSSolver.getSolution();
+			
+			int x=0; int y=0;
+			
+			for(int i=0;i<solution.length;i++) {
+				int step=solution[i];
+				
+				if(step==1) {
+					y--;
+					paths.add(new Path(display,x*display.getResolution(),y*display.getResolution()));
+				}else if(step==2) {
+					y++;
+					paths.add(new Path(display,x*display.getResolution(),y*display.getResolution()));
+				}else if(step==3) {
+					x--;
+					paths.add(new Path(display,x*display.getResolution(),y*display.getResolution()));
+				}else if(step==4) {
+					x++;
+					paths.add(new Path(display,x*display.getResolution(),y*display.getResolution()));
+				}
+			}
+			bFSSolver.stop();
+			bFSSolver=null;
+		}
+	}
+	
+	
+	public void render(Graphics2D g) {
+		
+		entrance.render(g);
+		
+		exit.render(g);
+		
 		for(int i=0;i<walls.size();i++) {
-			try {
-				walls.get(i).render(g);
-			}catch(Exception exception) {}
+			walls.get(i).render(g);
+		}
+		
+		for(int i=0;i<paths.size();i++) {
+			paths.get(i).render(g);
 		}
 		
 		for(int x=0;x<display.getWidth()/display.getResolution();x++) {
@@ -97,14 +134,13 @@ public class RunState extends State {
 				g.drawRect(x*display.getResolution(),y*display.getResolution(),display.getResolution(),display.getResolution());
 			}
 		}
-		
-		maker.render(g);
-		
-		exit.render(g);
-		
 	}
 	
 	public void reset() {
+		walls.removeAll(walls);
+		
+		paths.removeAll(paths);
+		
 		if(dFSMaker!=null) {
 			dFSMaker.stop();
 			dFSMaker=null;
@@ -114,66 +150,22 @@ public class RunState extends State {
 			bFSSolver.stop();
 			bFSSolver=null;
 		}
-		
-		if(aStarSolver!=null) {
-			aStarSolver.stop();
-			aStarSolver=null;
-		}
-		
-		if(walls.size()!=0) {
-			try {
-				walls.removeAll(walls);
-			}catch(Exception exception) {}
-		}
-		
-		if(paths.size()!=0) {
-			try {
-				paths.removeAll(paths);
-			}catch(Exception exception) {}
-		}
-		
-		maker.setX(0);
-		maker.setY(0);
-		maker.setIndex(0);
-		
-		exit.setX(0);
-		exit.setY(0);
 	}
 	
-	public LinkedList<Path> getPaths() {
-		return paths;
+	public void setDFSMaking(boolean DFSMaking) {
+		this.DFSMaking=DFSMaking;
 	}
 	
-	public void addPath(Path path) {
-		paths.add(path);
+	public boolean getDFSMaking() {
+		return DFSMaking;
 	}
 	
-	public void removePath(Path path) {
-		paths.remove(path);
+	public void setBFSSolving(boolean BFSSolving) {
+		this.BFSSolving=BFSSolving;
 	}
 	
-	public void removeAllPaths() {
-		paths.removeAll(paths);
-	}
-	
-	public ArrayList<Wall> getWalls() {
-		return walls;
-	}
-	
-	public void addWall(Wall wall) {
-		walls.add(wall);
-	}
-	
-	public void removeWall(Wall wall) {
-		walls.remove(wall);
-	}
-	
-	public Maker getMaker() {
-		return maker;
-	}
-	
-	public Exit getExit() {
-		return exit;
+	public boolean getBFSSolving() {
+		return BFSSolving;
 	}
 	
 	public DFSMaker getDFSMaker() {
@@ -182,5 +174,17 @@ public class RunState extends State {
 	
 	public BFSSolver getBFSSolver() {
 		return bFSSolver;
+	}
+	
+	public Entrance getEntrance() {
+		return entrance;
+	}
+	
+	public Exit getExit() {
+		return exit;
+	}
+	
+	public ArrayList<Wall> getWalls() {
+		return walls;
 	}
 }
