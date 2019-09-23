0feb26b57403d1e918a245a7d17244b78eba7a52
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-23 12:59:06 -0700

    Update Exit.java

--- src/Entity/Exit.java
+++ src/Entity/Exit.java
@@ -3,32 +3,35 @@ package Entity;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import Interface.Renderable;
+import Interface.Tickable;
 import Main.Display;
 
-public class Exit extends Entity {
-	
-	private final Color color=Color.BLUE;
-	
+public class Exit extends Entity implements Tickable,Renderable {
 	public Exit(Display display,int x,int y) {
 		super(display,x,y);
 	}
 	
 	public void tick() {
-		if(display.getMouseActionHandler().getPressed()) {
-			if(display.getKeyActionHandler().getMoveExit()==true) {
+		if(display.getKeyActionHandler().getMoveExit()) {
+			if(display.getMouseActionHandler().getPressed()) {
 				int x=display.getMouseMotionHandler().getX()/display.getResolution();
 				int y=display.getMouseMotionHandler().getY()/display.getResolution();
-				setX(x*display.getResolution());
-				setY(y*display.getResolution());
+				
+				this.x=x*display.getResolution();
+				this.y=y*display.getResolution();
 			}
 		}
 	}
 	
 	public void render(Graphics2D g) {
-		if(display.getRunState().getMaker().getX()==x && display.getRunState().getMaker().getY()==y) {
+		
+		Entrance entrance=display.getRunState().getEntrance();
+		
+		if(entrance.getX()==x && entrance.getY()==y) {
 			g.setColor(Color.GREEN);
 		}else {
-			g.setColor(color);
+			g.setColor(Color.BLUE);
 		}
 		g.fillRect(x,y,display.getResolution(),display.getResolution());
 	}
