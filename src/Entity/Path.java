e6c1694661ed0389315e6e76de674e3e32c3b1c3
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-23 12:59:30 -0700

    Update Path.java

--- src/Entity/Path.java
+++ src/Entity/Path.java
@@ -3,22 +3,16 @@ package Entity;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import Interface.Renderable;
 import Main.Display;
 
-public class Path extends Entity {
-	
-	private final Color color=Color.PINK;
-	
+public class Path extends Entity implements Renderable {
 	public Path(Display display,int x,int y) {
 		super(display,x,y);
 	}
 	
-	public void tick() {
-		
-	}
-	
 	public void render(Graphics2D g) {
-		g.setColor(color);
+		g.setColor(Color.ORANGE);
 		g.fillRect(x,y,display.getResolution(),display.getResolution());
 	}
 }
