ab6a48f14321bb8948e76fd824e9e571519d739b
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-23 12:59:48 -0700

    Update Wall.java

--- src/Entity/Wall.java
+++ src/Entity/Wall.java
@@ -3,32 +3,21 @@ package Entity;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import Interface.Renderable;
+import Interface.Tickable;
 import Main.Display;
 
-public class Wall extends Entity {
-	
-	private final Color color=Color.GRAY;
-	
+public class Wall extends Entity implements Tickable,Renderable {
 	public Wall(Display display,int x,int y) {
 		super(display,x,y);
 	}
 	
 	public void tick() {
-		if(display.getMouseActionHandler().getPressed()) {
-			if(display.getKeyActionHandler().getRemoveWall()) {
-				int mouseX=display.getMouseMotionHandler().getX()/display.getResolution();
-				
-				int mouseY=display.getMouseMotionHandler().getY()/display.getResolution();
-				
-				if(x==mouseX*display.getResolution() && y==mouseY*display.getResolution()) {
-					display.getRunState().removeWall(this);
-				}
-			}
-		}
+		
 	}
 	
 	public void render(Graphics2D g) {
-		g.setColor(color);
+		g.setColor(Color.DARK_GRAY);
 		g.fillRect(x,y,display.getResolution(),display.getResolution());
 	}
 }
