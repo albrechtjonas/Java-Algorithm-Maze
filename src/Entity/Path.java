d90d050e4926b29ad91e4b19fdbab9fcdda79fa2
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-28 17:15:13 -0700

    Update Path.java

--- src/Entity/Path.java
+++ src/Entity/Path.java
@@ -4,15 +4,32 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import Interface.Renderable;
+import Interface.Tickable;
 import Main.Display;
 
-public class Path extends Entity implements Renderable {
-	public Path(Display display,int x,int y) {
+public class Wall extends Entity implements Tickable,Renderable {
+	public Wall(Display display,int x,int y) {
 		super(display,x,y);
 	}
 	
+	public void tick() {
+		if(display.getKeyActionHandler().getRemoveWall()) {
+			if(display.getMouseActionHandler().getPressed()) {
+				int x=display.getMouseMotionHandler().getX();
+				int y=display.getMouseMotionHandler().getY();
+				
+				if(x>this.x && x<this.x+display.getResolution()) {
+					if(y>this.y && y<this.y+display.getResolution()) {
+						display.getRunState().getEntities()[x/display.getResolution()][y/display.getResolution()]=null;
+					}
+				}
+				
+			}
+		}
+	}
+	
 	public void render(Graphics2D g) {
-		g.setColor(Color.ORANGE);
-		g.fillRect(x,y,display.getResolution(),display.getResolution());
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(x,y,display.getResolution(),display.getResolution());	
 	}
 }
